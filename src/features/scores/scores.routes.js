7a032199c374ea1360978b55faaf57789606d6f1
Murewa Ajala <murewatajala@gmail.com>
2026-08-16 22:47:39 +0100

    Allow guest access to leaderboard

--- src/features/scores/scores.routes.js
+++ src/features/scores/scores.routes.js
@@ -3,12 +3,14 @@ const router = express.Router();
 const scoresController = require('./scores.controller');
 const authenticate = require('../../middleware/authenticate');
 
-// All score routes require authentication
+// Public routes
+router.get('/leaderboard/top', scoresController.getLeaderboard);
+
+// All other score routes require authentication
 router.use(authenticate);
 
 router.post('/', scoresController.submitScore);
 router.get('/', scoresController.getScores);
-router.get('/leaderboard/top', scoresController.getLeaderboard);
 router.get('/:roomId', scoresController.getRoomScore);
 
 module.exports = router;
